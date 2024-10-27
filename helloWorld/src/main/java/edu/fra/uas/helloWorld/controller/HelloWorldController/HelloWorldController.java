ace24488a8b5ff8d076a632eef74590cf60a2b73
davidAlphaBetaGamma <david.gerling@stud.fra-uas.de>
2024-10-28 00:40:01 +0100

    #

--- helloWorld/src/main/java/edu/fra/uas/helloWorld/controller/HelloWorldController/HelloWorldController.java
+++ helloWorld/src/main/java/edu/fra/uas/helloWorld/controller/HelloWorldController/HelloWorldController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloWorldController {
 
-    @RequestMapping(value="/x", method=RequestMethod.GET)
+    @RequestMapping(value="/", method=RequestMethod.GET)
     @ResponseBody
     public String getHelloWorld() {
         return "<h1>Hello World in Spring Boot 2024!</h1>";
