70fb905d323637cc4f5aa23f535cf484bd8cd77c
davidAlphaBetaGamma <david.gerling@fra-uas.de>
2024-11-21 15:55:09 +0100

    #

---
+++ Notendurchschnitt/src/main/java/com/example/Notendurchschnitt/model/Note.java
@@ -0,0 +1,6 @@
+package main.java.com.example.Notendurchschnitt.model;
+
+public class Note {
+
+
+}
---
+++ Notendurchschnitt/src/test/java/com/example/Notendurchschnitt/NotendurchschnittApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Notendurchschnitt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotendurchschnittApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
