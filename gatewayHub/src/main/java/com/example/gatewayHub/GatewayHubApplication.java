886f0507166aa47b4959936aa550dceb174485d9
davidAlphaBetaGamma <150674049+davidAlphaBetaGamma@users.noreply.github.com>
2024-12-19 14:40:24 +0100

    Add files via upload

---
+++ gatewayHub/src/main/java/com/example/gatewayHub/GatewayHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.gatewayHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayHubApplication.class, args);
+	}
+
+}
---
+++ gatewayHub/src/test/java/com/example/gatewayHub/GatewayHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gatewayHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
